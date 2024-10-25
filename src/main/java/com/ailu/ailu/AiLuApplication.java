ed309c8ca1fc8c8e7366a333a2b1fd395ac203af
m0_73095030 <m0_73095030@noreply.gitcode.com>
2024-10-25 11:13:48 +0800

    Initial commit

---
+++ src/main/java/com/ailu/ailu/AiLuApplication.java
@@ -0,0 +1,13 @@
+package com.ailu.ailu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLuApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ailu/ailu/service/agent/AgentService.java
@@ -0,0 +1,10 @@
+package com.ailu.ailu.service.agent;
+
+/**
+ * @Description:
+ * @Author: ailu
+ * @Date: 2024/10/25 上午11:06
+ */
+
+public class AgentService {
+}
